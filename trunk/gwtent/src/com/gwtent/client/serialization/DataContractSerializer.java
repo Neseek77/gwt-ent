54c909026214e62209f00dad07aaba73e04cc611
JamesLuo.au <JamesLuo.au@2214c72b-7541-0410-9e57-a9dfcfa04591>
2009-03-31 11:43:28 +0000

    JSON bug fixes

--- trunk/gwtent/src/com/gwtent/client/serialization/DataContractSerializer.java
+++ trunk/gwtent/src/com/gwtent/client/serialization/DataContractSerializer.java
@@ -1,10 +1,7 @@
 package com.gwtent.client.serialization;
 
-import com.gwtent.client.common.ObjectFactory;
-import com.gwtent.client.reflection.ClassType;
 
 public interface DataContractSerializer {
-	public <T extends Object> T deserializeObject(String json, Class<T> clazz, ObjectFactory<Object> objectFactory);
-	public <T extends Object> T deserializeObject(String json, Class<T> clazz, ObjectFactory<Object> objectFactory, DoubleConvert doubleConvert);
+	public <T extends Object> T deserializeObject(String json, Class<T> clazz);
 	public String serializeObject(Object object);
 }
