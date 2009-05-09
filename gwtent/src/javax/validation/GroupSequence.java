e24de1c90077c2b269205f41644d16d463db3046
JamesLuo.au <JamesLuo.au@2214c72b-7541-0410-9e57-a9dfcfa04591>
2009-05-09 13:54:25 +0000

    Update to new version of JSR303

--- gwtent/src/javax/validation/GroupSequence.java
+++ gwtent/src/javax/validation/GroupSequence.java
@@ -6,15 +6,16 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import java.lang.annotation.Target;
 
 /**
- * <pre>--
- * As soon as the classes in javax.validation are available from official sites, this
- * class will be removed from this compilation unit.
- * --</pre>
- */
-@Target({TYPE})
+* Define a group sequence
+* The interface hosting @GroupSequence is representing the group sequence.
+* When hosted on a class, represents the <code>Default</code> group
+* for that class.
+*
+* @author Emmanuel Bernard
+* @author Hardy Ferentschik
+*/
+@Target({ TYPE })
 @Retention(RUNTIME)
 public @interface GroupSequence {
-    String name();
-
-    String[] sequence();
+	Class<?>[] value();
 }
