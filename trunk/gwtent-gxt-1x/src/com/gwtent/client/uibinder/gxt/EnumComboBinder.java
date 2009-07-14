f02d66cdfe2fc79aa4913401e45d4a6257eab85f
JamesLuo.au <JamesLuo.au@2214c72b-7541-0410-9e57-a9dfcfa04591>
2009-07-14 02:12:31 +0000

    gwtent-gxt, support ext gxt 1.x version

---
+++ trunk/gwtent-gxt-1x/src/com/gwtent/client/uibinder/gxt/EnumComboBinder.java
@@ -0,0 +1,70 @@
+package com.gwtent.client.uibinder.gxt;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.extjs.gxt.ui.client.Events;
+import com.extjs.gxt.ui.client.event.FieldEvent;
+import com.extjs.gxt.ui.client.event.Listener;
+import com.extjs.gxt.ui.client.widget.form.SimpleComboValue;
+import com.gwtent.client.common.ObjectFactory;
+import com.gwtent.client.uibinder.AbstractUIBinder;
+import com.gwtent.client.uibinder.IBinderMetaData;
+import com.gwtent.client.uibinder.ModelValue;
+import com.gwtent.client.uibinder.UIBinder;
+import com.gwtent.client.uibinder.AbstractUIBinder.EditorToValueSetException;
+import com.gwtent.client.uibinder.gxt.widget.EnumCombo;
+
+public class EnumComboBinder<T extends Enum<T>> extends AbstractUIBinder<EnumCombo<T>, T> {
+
+	public static class BinderMetaData<T extends Enum<T>> implements IBinderMetaData<EnumCombo<T>, T>{
+
+    public Class<?>[] getSupportedEditors() {
+      return new Class<?>[]{EnumCombo.class};
+    }
+
+    public ObjectFactory<UIBinder<EnumCombo<T>, T>> getFactory() {
+      return new ObjectFactory<UIBinder<EnumCombo<T>, T>>(){
+
+        public UIBinder<EnumCombo<T>, T> getObject() {
+          return new EnumComboBinder<T>();
+        }};
+    }
+
+    public Class<?>[] getSupportedValueTypes() {
+      return new Class<?>[]{};
+    }
+  }
+	
+	protected void doInit(EnumCombo<T> widget, ModelValue<T> value) {
+		widget.updateComboSelectList((Class<T>) value.getValueClass());
+		
+		widget.addListener(Events.Valid, new Listener<FieldEvent>(){
+
+      public void handleEvent(FieldEvent be) {
+      	if (getWidget().getSelection().size() > 0){
+      		SimpleComboValue<T> value = getWidget().getSelection().get(0);
+					setEditorToValue(value.getValue());
+      		return;
+      	}
+      	
+				setEditorToValue(null);
+
+      }});
+	}
+
+	@Override
+	protected void setValueToEditor(T value, EnumCombo<T> widget) {
+		List<SimpleComboValue<T>> values = new ArrayList<SimpleComboValue<T>>();
+		
+		for (int i = 0; i < widget.getStore().getCount(); i++){
+			SimpleComboValue<T> widgetValue = widget.getStore().getAt(i);
+			if (widgetValue.getValue() == value){
+				values.add(widgetValue);
+			}
+		}
+		
+		widget.setSelection(values);
+	}
+
+}
