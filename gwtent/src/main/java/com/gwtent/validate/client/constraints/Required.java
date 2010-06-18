4a708ff869d42574eca6f3e38fa633f5852ac356
JamesLuo.au <JamesLuo.au@2214c72b-7541-0410-9e57-a9dfcfa04591>
2010-06-18 07:00:46 +0000

    Change to Maven structure

---
+++ gwtent/src/main/java/com/gwtent/validate/client/constraints/Required.java
@@ -0,0 +1,21 @@
+package com.gwtent.validate.client.constraints;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Target;
+import java.lang.annotation.Retention;
+
+import javax.validation.Constraint;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+
+@Documented
+@Constraint(validatedBy = RequiredValidator.class)
+@Target( { METHOD, FIELD })
+@Retention(RUNTIME)
+public @interface Required {
+	Class<?>[] groups() default {};
+
+	String message() default "{constraint_required}";
+}
